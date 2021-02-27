bbeda4b6e5658474d41c8d5614bcd0c5ebf75638
Jeff Hall <jeff.leon.hall@gmail.com>
2021-02-27 16:45:58 -0700

    added a 'table' product into the table'

--- queries_products.sql
+++ queries_products.sql
@@ -1,10 +1,14 @@
 -- Add a product with a name of 'chair', price of 44.00 and can_be_returned of false
- INSERT INTO products (name, price, can_be_returned)               
+INSERT INTO products (name, price, can_be_returned)               
     VALUES ('chair', 44.00, false);
 
 -- Add a product with a name of 'stool', price of 25.99 and can_be_returned of true
 INSERT INTO products (name, price, can_be_returned) 
     VALUES ('stool', 25.99, true);
+
+-- Add a product with a name of 'table', price of 124.00 and can_be_returned of false
+INSERT INTO products (name, price, can_be_returned)
+    VALUES ('table', 124.00, false);
 
 
 
