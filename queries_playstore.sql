db120aced2bbba9fb14a0fc189906e24e5728a9f
Jeff Hall <jeff.leon.hall@gmail.com>
2021-02-27 18:26:19 -0700

    Select the id and app name for all apps that were updated on August 1, 2018

--- queries_playstore.sql
+++ queries_playstore.sql
@@ -1,2 +1,6 @@
 -- Select the app with an ID of 1880
 SELECT * FROM analytics WHERE id = 1880;
+
+-- Select the id and app name for all apps that were updated on August 1, 2018
+SELECT id, app_name FROM analytics WHERE last_updated = '2018-08-01';
+
