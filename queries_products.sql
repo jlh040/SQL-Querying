397e00126d307b1c3fec35e83c6275f7c660e3da
Jeff Hall <jeff.leon.hall@gmail.com>
2021-02-27 17:08:42 -0700

    select products with a price between 22.50 and 99.99

--- queries_products.sql
+++ queries_products.sql
@@ -29,6 +29,8 @@ SELECT * FROM products WHERE can_be_returned = true;
 -- Select only the products that have a price less than 44.00
 SELECT * FROM products WHERE price < 44;
 
+-- Select products that have a price between 22.50 and 99.99;
+SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
 
 
 
