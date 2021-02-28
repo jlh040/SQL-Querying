49f6b94654fab200284951f6cce438124152e9ea
Jeff Hall <jeff.leon.hall@gmail.com>
2021-02-27 18:30:43 -0700

    Find the number of apps that are in each category

--- queries_playstore.sql
+++ queries_playstore.sql
@@ -4,3 +4,5 @@ SELECT * FROM analytics WHERE id = 1880;
 -- Select the id and app name for all apps that were updated on August 1, 2018
 SELECT id, app_name FROM analytics WHERE last_updated = '2018-08-01';
 
+-- Find the number of apps that are in each category
+SELECT category, COUNT(*) FROM analytics GROUP BY category;
