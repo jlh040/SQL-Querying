2a5efb27d17a956571d9a33a08b6f50fae5c8520
Jeff Hall <jeff.leon.hall@gmail.com>
2021-02-27 17:13:50 -0700

    take 20 off of every product's price

--- queries_products.sql
+++ queries_products.sql
@@ -32,6 +32,9 @@ SELECT * FROM products WHERE price < 44;
 -- Select products that have a price between 22.50 and 99.99;
 SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
 
+-- Take 20 off of every product in the table
+UPDATE products SET price = price - 20;
+
 
 
 
